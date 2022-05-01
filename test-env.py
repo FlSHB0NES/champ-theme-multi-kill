d3b4193809c02b16951302b8adfee8cda1ee21b2
Ethan Chiang (Student) <echian@uwo.ca>
2022-05-01 18:55:57 -0400

    now loops in game and prints events that transpire in game

--- test-env.py
+++ test-env.py
@@ -9,18 +9,30 @@ import json
 from riotwatcher import LolWatcher, ApiError
 import pandas as pd
 import pprint
+import time
 
+def print_events(dict):
+
+    for event in dict:
+        print(event['EventName'])
+        
 # USER INFORMATION
 
 api_key = "RGAPI-e8615c8d-9508-4e7c-b63e-74dd8f7e3974"
 my_region = 'na1'
 summoner_name = 'FlSHBONES'
-query = 'allgamedata'
+query = 'eventdata'
 cert_path = './riot/riotgames.pem'
 
-# print("API-key\t\t: " + api_key + "\nRegion\t\t: " + my_region + "\nSummoner\t: " + summoner_name)
+stored_events = {}
 
-# SET UP WATCHER
-#GET https://127.0.0.1:2999/liveclientdata/allgamedata
-request = requests.get(('https://127.0.0.1:2999/liveclientdata/' + query), verify=cert_path)
-print(request)
+while(True):
+
+    # GET DATA
+    response = requests.get(('https://127.0.0.1:2999/liveclientdata/' + query), verify=cert_path)
+    events = json.loads(response.text)['Events']
+
+    print_events(events)
+    
+    time.sleep(1)
+
